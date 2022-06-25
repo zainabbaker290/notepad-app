fd6b0adb7b9a14e88c6768ee5407a42e6836d253
zainabbaker290 <119340513@umail.ucc.ie>
2022-06-25 22:36:26 +0100

    removed key

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/notes", methods=["GET"])
+@app.route("/notes", methods=["GET", "POST"])
 def all_notes():
     if request.method == "GET":
         notes_file = open("notes.json", "r")
         notes_object = json.load(notes_file)
         notes_file.close()
-        notes_list = []
         for note in notes_object["notes"]:
-            note_result = "[note id: "+  note["id"] + ", note title: " + note["title"] + ", created: " + note["created"] + ", modified: " + note["modified"] + "]"
-            notes_list.append(note_result)
-        return json.dumps(notes_list)
-
+            note.pop("body")
+        return json.dumps(notes_object)
+    
 @app.route("/notes/<notes_id>", methods=["GET"])
 def get_note(notes_id):
     notes_file = open("notes.json", "r")
